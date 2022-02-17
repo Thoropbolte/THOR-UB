89d1fe792b9ed55b32b7d766522a6c89261f051c
MOHAMMAD AMAAN <amaankhan66666@gmail.com>
2022-02-17 13:35:38 +0530

    Update extras.py

--- userbot/helpers/extras.py
+++ userbot/helpers/extras.py
@@ -80,7 +80,7 @@ async def edit_or_reply(
 
 
 # delete timeout
-async def delete_thor(event, text, time=None, parse_mode=None, link_preview=None):
+async def delete_thorbot(event, text, time=None, parse_mode=None, link_preview=None):
     parse_mode = parse_mode or "md"
     link_preview = link_preview or False
     time = time or 10
